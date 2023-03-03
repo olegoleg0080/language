ad5ef93586493ff55984415550987da3ae4ee606
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-03-03 13:52:38 +0200

    skill

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("[lock"),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
