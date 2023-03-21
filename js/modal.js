fd8c13bf57bac22a054ae7f2d32f30c2faf5d099
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-03-21 20:34:23 +0200

    madal

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,16 @@
 (() => {
     const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-    body: document.querySelector("[lock"),
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+      body: document.querySelector("[lock]"),
     };
-
+  
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  
     function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    refs.body.classList.toggle("lock");
+      refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("lock")
     }
-})();
+  })();
